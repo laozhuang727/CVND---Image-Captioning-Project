6b85ecd4b75fcff3f5fb7de97a06e3d731e4d713
ryan.zhuang <laozhuang727@hotmail.com>
2018-12-23 22:30:32 +0800

    first init

--- model.py
+++ model.py
@@ -23,11 +23,49 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.hidden_dim = hidden_size
+
+        # embedding layer that turns words into a vector of a specified size
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+
+        # the LSTM takes embedded word vectors (of a specified size) as inputs
+        # and outputs hidden states of size hidden_dim
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+
+        # initialize the hidden state (see code below)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        ''' At the start of training, we need to initialize a hidden state;
+           there will be none because the hidden state is formed based on perviously seen data.
+           So, this function defines a hidden state with all zeroes and of a specified size.'''
+        # The axes dimensions are (n_layers, batch_size, hidden_dim)
+        return (torch.zeros(1, 1, self.hidden_dim),
+                torch.zeros(1, 1, self.hidden_dim))
     
     def forward(self, features, captions):
-        pass
+        # create embedded word vectors for each word in a captions
+        embeds = self.word_embeddings(captions)
+
+        # get the output and hidden state by passing the lstm over our word embeddings
+        # the lstm takes in our embeddings and hiddent state
+        # lstm_out, self.hidden = self.lstm(
+        #     embeds.view(len(captions), 1, -1), self.hidden)
+
+        # LSTM 返回的第一个值是序列中的所有隐藏状态。
+        # 第二个是最近的隐藏状态
+
+        # 添加额外的第二个维度
+        inputs = torch.cat(embeds).view(len(embeds), 1, -1)
+        lstm_out, self.hidden = self.lstm(inputs, self.hidden)
+
+        return lstm_out
+
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        predicted_sentence = model(inputs)
+
+        print('\n')
+        print('Predicted tags: \n', predicted_sentence)
